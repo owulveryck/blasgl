180fcd9bb6ba88c1f115ff57b1a644655c1afc9f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-09-09 21:38:15 +0200

    feat: starting the different implementations

--- blasgl.go
+++ blasgl.go
@@ -177,12 +177,6 @@ func (i *Implementation) Sspr2(ul blas.Uplo, n int, alpha float32, x []float32, 
 
 }
 
-// Sgemm ...
-func (i *Implementation) Sgemm(tA blas.Transpose, tB blas.Transpose, m int, n int, k int, alpha float32, a []float32, lda int, b []float32, ldb int, beta float32, c []float32, ldc int) {
-	gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
-
-}
-
 // Ssymm ...
 func (i *Implementation) Ssymm(s blas.Side, ul blas.Uplo, m int, n int, alpha float32, a []float32, lda int, b []float32, ldb int, beta float32, c []float32, ldc int) {
 	gonumImpl.Ssymm(s, ul, m, n, alpha, a, lda, b, ldb, beta, c, ldc)
