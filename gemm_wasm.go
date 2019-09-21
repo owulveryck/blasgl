cba1858ae5def075727f47a596c6edd9fa6a18cc
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-09-21 08:22:15 +0200

    chore: prepare the best case to apply weblas.sgemm

--- gemm_wasm.go
+++ gemm_wasm.go
@@ -2,11 +2,27 @@
 
 package blasgl
 
-import "gonum.org/v1/gonum/blas"
+import (
+	"gonum.org/v1/gonum/blas"
+)
 
 // Sgemm ...
 func (Implementation) Sgemm(tA blas.Transpose, tB blas.Transpose, m int, n int, k int, alpha float32, a []float32, lda int, b []float32, ldb int, beta float32, c []float32, ldc int) {
-	gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	switch {
+	case tA != blas.NoTrans:
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	case tB != blas.NoTrans:
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	case tA == blas.NoTrans && m != lda:
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	case tB == blas.NoTrans && k != ldb:
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	case m != ldc:
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	default:
+		// TODO use the webladgl here
+		gonumImpl.Sgemm(tA, tB, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+	}
 }
 
 // Dgemm ...
